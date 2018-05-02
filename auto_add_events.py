102a237c3ce5d2e43a915cd678f34f00b58dd7a7
Kevin <kmcgregr@gmail.com>
2018-05-02 19:21:21 -0400

    first attempt to dynamic build json object

--- auto_add_events.py
+++ auto_add_events.py
@@ -1,15 +1,15 @@
 
 from __future__ import print_function
-from apiclient.discovery import build
-from httplib2 import Http
-from oauth2client import file, client, tools
-import datetim
+#from apiclient.discovery import build
+#from httplib2 import Http
+#from oauth2client import file, client, tools
+import datetime
 import json
 import sys
 import StringIO
 
 
-def setup_calendar_api:
+def setup_calendar_api():
     SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
     store = file.Storage('credentials.json')
     creds = store.get()
@@ -18,18 +18,37 @@ def setup_calendar_api:
         creds = tools.run_flow(flow, store)
     service = build('calendar', 'v3', http=creds.authorize(Http()))
 
-def build_json():
+def get_date_info():
     for line in sys.stdin:
         event_data = line.strip().split(",")
         event_date,event_time,event_title,event_location = event_data
-
+    json_date = json.dumps(build_json(event_date,event_time,event_title,event_location))
+    print (json_date)
     print (event_time, event_title)
+    print (event_data[0])
+  
+
+def build_json(event_date,event_time,event_title,event_location):
+    json_data = {}
+    json_data['summary'] = event_title
+
+    json_start_data = {}
+    json_start_data['dateTime'] = event_date + ":" + event_time
+
+    json_data['start'] = json_start_data
+
+    json_end_data = {}
+    json_end_data['dateTime'] = event_date + ":" + event_time
+
+    json_data['end'] = json_end_data
+
+    return json_data
 
 test_text = """18-05-10,6:00,Soccer Match,Oakridge HS Mini Field - West"""
 def main():
 	
 	sys.stdin = StringIO.StringIO(test_text)
-	build_json()
+	get_date_info()
 	sys.stdin = sys.__stdin__
 
 main()
